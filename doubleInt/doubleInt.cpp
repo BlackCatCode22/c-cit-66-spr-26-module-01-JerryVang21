3e2dcd451ba9b34950052b63d18cc476e4f5101f
Jerry Vang <0864774@MY.SCCCD.EDU>
2026-01-14 20:31:23 -0800

    Add doubleInt.cpp

---
+++ doubleInt/doubleInt.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int number;
+    cout << "Enter an integer: ";
+    cin >> number;
+    cout << "Double value: " << number * 2 << endl;
+    return 0;
+}
+//
