b6f1849f87e1e21cb4ba10da48842e590f752848
Jerry Vang <0864774@MY.SCCCD.EDU>
2026-01-19 14:18:33 -0800

    Add tripleInt.cpp

---
+++ tripleInt/tripleInt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int number;
+    cout << "Enter an integer: ";
+    cin >> number;
+    cout << "Triple value:" << number * 3 << endl;
+    return 0;
+}
+// Created by jerry on 1/19/2026.
+//
