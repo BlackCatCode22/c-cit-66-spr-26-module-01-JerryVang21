c09e5a41b5fad9a645a6dd5bb21c339e355f051f
Jerry Vang <0864774@MY.SCCCD.EDU>
2026-01-23 19:59:12 -0800

    Add sumTwoInts.cpp

---
+++ sumTwoInts/sumTwoInts.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int firstNum;
+    int secondNum;
+    int sum;
+
+    cout << "Enter the first number: ";
+    cin >> firstNum;
+
+    cout << "Enter the second number: ";
+    cin >> secondNum;
+
+    sum = firstNum + secondNum;
+
+    cout << "the sum is: " << sum << endl;
+
+    return 0;
+}
+// Created by jerry on 1/23/2026.
+//
